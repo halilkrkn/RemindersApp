abdb10abdcaf733f0d8c89b14a3eaf7544ea7b90
halilkrkn <42476890+halilkrkn@users.noreply.github.com>
2020-12-13 17:21:44 +0300

    Reminder App started.
    Tasarım oluşturuldu ve geçici veri içerisindende görünüme dinamiklik kazandırıldı.

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { Text, StyleSheet, View, TouchableOpacity,FlatList } from 'react-native'
+import {AntDesign} from '@expo/vector-icons'
+import colors from './Colors';
+import tempData from '../service/tempData'
+import ReminderList from '../components/ReminderList'       
+
+export default class HomeScreen extends React.Component {
+    render() {
+        return (
+            <View style = {styles.container}>
+                <View style={{flexDirection:"row"}}>
+                    <View style={styles.divider}/>
+                    <Text style={styles.title}>
+                        Reminders <Text style={{fontWeight:"300",color:colors.blue}}>Lists</Text>
+                    </Text>
+                    <View style={styles.divider}/>
+                </View>
+                
+                <View style = {styles.divider}>
+                    <TouchableOpacity style={styles.addList}>
+                        <AntDesign name="plus" size={16} color={colors.blue}/>
+                    </TouchableOpacity>
+                    <Text style= {styles.add}>Add Reminder</Text>
+                </View>
+            
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+    container:{
+        flex:1,
+        backgroundColor:"#fff",
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    
+    divider: {
+
+        backgroundColor: colors.lightBlue,
+        height:1,
+        flex:1,
+        alignSelf:"center"
+    },
+    title: {
+
+        fontSize: 40,
+        fontWeight:"800",
+        color:colors.black,
+        paddingHorizontal:64
+
+    },
+
+    addList: {
+        borderWidth: 2,
+        borderColor:colors.lightBlue,
+        borderRadius:6,
+        padding:16,
+        alignItems: "center",
+        justifyContent:"center"
+    },
+    
+    add: {
+        color:colors.blue,
+        fontWeight: "600",
+        fontSize: 15,
+        marginTop: 8
+    }
+
+})
---
+++ src/service/tempData.js
@@ -0,0 +1,98 @@
+// Temporary Data until we connect with firebase 
+
+export default tempData = [
+    {
+    name: "Plan a Trip",
+    color: "#23ee55",
+    todos: [
+        {
+
+            title: "Book Flight",
+            completed: false
+        },
+
+        {
+
+            title: "Passport Check",
+            completed: true
+        },
+
+        {
+
+            title: "Reverse Hotel Room",
+            completed: true
+        },
+
+        {
+
+            title: "Pack Luggage",
+            completed: false
+        }
+    
+    ]
+},
+{
+    name: "Errands",
+    color: "#fbbb",
+    todos: [
+        {
+
+            title: "Buy Milk",
+            completed: false
+        },
+
+        {
+
+            title: "Friends Travel",
+            completed: false
+        },
+
+        {
+
+            title: "Reverse Restaurant",
+            completed: true
+        },
+
+        {
+
+            title: "Learn English",
+            completed: true
+        }
+    
+    ]
+},
+
+{
+    name: "Birthday Party",
+    color: "#0ce",
+    todos: [
+        {
+
+            title: "Sent Motors",
+            completed: true
+        },
+
+        {
+
+            title: "Go Home",
+            completed: true
+        },
+
+        {
+
+            title: "Go to School",
+            completed: true
+        },
+
+        {
+
+            title: "Play Apex Legends",
+            completed: false
+        }
+    
+    ]
+}
+
+
+
+]
